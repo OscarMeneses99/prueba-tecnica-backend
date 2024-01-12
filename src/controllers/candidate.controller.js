33872ccbecdf7e753a9bea180399d6491afff8ac
Oscar Meneses <omenesess1800@alumno.ipn.mx>
2024-01-12 15:52:58 -0600

    Refactor candidate controller with error handling

--- src/controllers/candidate.controller.js
+++ src/controllers/candidate.controller.js
@@ -6,34 +6,52 @@ import {
 
 export class CandidateController {
   static async getCandidates(req, res) {
-    const candidates = await CandidateModel.getAll();
-    res.status(200).json(candidates);
+    try {
+      const candidates = await CandidateModel.getAll();
+      res.status(200).json(candidates);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
   }
   static async createCandidate(req, res) {
-    const validationResult = validateCandidate(req.body);
-    if (!validationResult.success)
-      res.status(400).json({ message: validationResult.error });
-    const createdCandidate = await CandidateModel.create({ validationResult });
-    res.status(201).json(createdCandidate);
+    try {
+      const validationResult = validateCandidate(req.body);
+      if (!validationResult.success)
+        res.status(400).json({ message: validationResult.error });
+      const createdCandidate = await CandidateModel.create({
+        validationResult,
+      });
+      res.status(201).json(createdCandidate);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
   }
   static async updateCandidate(req, res) {
-    const { id } = req.params;
-    const validationResult = validatePartialCandidate(req.body);
-    if (!validationResult.success)
-      res.status(422).json({ error: validationResult.error.message });
-    const updatedCandidate = await CandidateModel.update({
-      id,
-      validationResult,
-    });
-    if (!updatedCandidate)
-      res.status(404).json({ message: "Candidate not found" });
-    res.status(200).json(updatedCandidate);
+    try {
+      const { id } = req.params;
+      const validationResult = validatePartialCandidate(req.body);
+      if (!validationResult.success)
+        res.status(422).json({ error: validationResult.error.message });
+      const updatedCandidate = await CandidateModel.update({
+        id,
+        validationResult,
+      });
+      if (!updatedCandidate)
+        res.status(404).json({ message: "Candidate not found" });
+      res.status(200).json(updatedCandidate);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
   }
   static async deleteCandidate(req, res) {
-    const { id } = req.params;
-    const deletedCandidate = await CandidateModel.delete({ id });
-    if (!deletedCandidate)
-      res.status(404).json({ message: "Candidate not found" });
-    res.status(200).json({ message: "Candidate deleted" });
+    try {
+      const { id } = req.params;
+      const deletedCandidate = await CandidateModel.delete({ id });
+      if (!deletedCandidate)
+        res.status(404).json({ message: "Candidate not found" });
+      res.status(200).json({ message: "Candidate deleted" });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
   }
 }
